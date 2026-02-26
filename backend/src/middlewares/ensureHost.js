36b447e95bcc5e4b6c5d1450c66623a596cafe9d
Meet Bajaj <147529332+Meet-Bajaj@users.noreply.github.com>
2026-02-26 12:18:18 +0530

    Delete backend directory

--- backend/src/middlewares/ensureHost.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (req, res, next) {
-  if (req.session && req.session.user && 
-      (req.session.user.role === "host" || req.session.user.role === "both")) {
-    return next();
-  }
-
-  return res.status(403).json({ error: "Host only" });
-};
